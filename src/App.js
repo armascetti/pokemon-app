d61df5f9ead69b6b6d35ca5817585158dfd9d276
Mandy_Mascetti <armascetti@gmail.com>
2022-11-08 12:38:22 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [pokemonName, setPokemonName] = useState("");
+  const [pokemonChosen, setPokemonChosen] = useState(false);
+  const [pokemon, setPokemon] = useState({
+    name: "",
+    species: "",
+    img: "",
+    hp: "",
+    attack: "",
+    defense: "",
+    type: "",
+  })
+
+  const searchPokemon = () => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}
+  `).then((response) => {
+      setPokemon({
+        name: pokemonName, species: response.data.species.name,
+        img: response.data.sprites.front_default,
+        hp: response.data.stats[0].base_stat,
+        attack: response.data.stats[1].base_stat,
+        defense: response.data.stats[2].base_stat,
+        type: response.data.types[0].type.name,
+      });
+      setPokemonChosen(true);
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="titleSection">
+        <h1> Pokemon Stats </h1>
+        <input type="text"
+          onChange={(event) => { setPokemonName(event.target.value) }}></input>
+        <button onClick={searchPokemon}> Search Pokemon </button>
+      </div>
+      <div className="displaySection">
+        {!pokemonChosen ? (
+          <h1> Please Choose a Pokemon</h1>
+        ) : (
+          <>
+            <h1>{pokemon.name}</h1>
+            <img src={pokemon.img} alt="pokechar" />
+            <h2> Species: {pokemon.species}</h2>           <h2> Type: {pokemon.type}</h2>
+          </>
+        )}
+      </div>
     </div>
   );
 }
-
 export default App;
